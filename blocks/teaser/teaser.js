6e6dcb7d3f03d7213ec78e03a35740962d9e09d4
jquintos214 <jeq214@gmail.com>
2026-08-16 11:45:36 -0700

    fixing teaser styles

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -3,7 +3,9 @@
  * @param {Element} block The teaser block element
  */
 export default function decorate(block) {
-  const [imageRow, titleRow, descriptionRow, actionsRow] = block.querySelectorAll(':scope > div');
+  const rows = [...block.querySelectorAll(':scope > div')];
+  const imageRow = rows.find((row) => row.querySelector('picture'));
+  const [titleRow, descriptionRow, actionsRow] = rows.filter((row) => row !== imageRow);
 
   const imgWrapper = imageRow?.querySelector(':scope > div');
   if (imgWrapper) {
@@ -22,6 +24,7 @@ export default function decorate(block) {
   const titleCol = titleRow?.querySelector(':scope > div');
   if (titleCol) {
     titleCol.querySelector('h1, h2, h3, h4, h5, h6')?.classList.add('teaser-title');
+    titleCol.querySelector(':scope > :not(h1, h2, h3, h4, h5, h6)')?.classList.add('teaser-pretitle');
     content.append(...titleCol.childNodes);
   }
 
